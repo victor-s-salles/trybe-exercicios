1ea7a6b478a1dac6bce898a14e64ebd20b2ae0f6
victor-s-salles <victor.salles@engenharia.ufjf.br>
2022-07-20 19:41:43 -0300

    Solução completa dos exercícios do dia

---
+++ fundamentos/bloco-04-Introdução-à-JavaScript/program08.js
@@ -0,0 +1,7 @@
+const a = 109;
+const b = 31;
+const c = 51;
+
+if ((a % 2 == 0) || (b % 2 == 0) || (c % 2 == 0)){
+    console.log(true)
+}else {console.log(false)}
---
+++ fundamentos/bloco-04-Introdução-à-JavaScript/program09.js
@@ -0,0 +1,7 @@
+const a = 100;
+const b = 30;
+const c = 50;
+
+if ((a % 2 != 0) || (b % 2 != 0) || (c % 2 != 0)){
+    console.log(true)
+}else {console.log(false)}
