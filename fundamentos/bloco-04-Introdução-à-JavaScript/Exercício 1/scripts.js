cfc7bbd9284af7a12d709475e850f5da99202199
victor-s-salles <victor.salles@engenharia.ufjf.br>
2022-07-20 15:38:21 -0300

    Começando os exercícios do D01B04

---
+++ fundamentos/bloco-04-Introdução-à-JavaScript/Exercício 1/scripts.js
@@ -0,0 +1,10 @@
+let myName = "Victor Salles";
+let birthCity = "Juiz de Fora";
+const birthYear = 2000;
+
+birthYear = 2030;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
---
+++ fundamentos/bloco-04-Introdução-à-JavaScript/Exercício 2/scripts.js
@@ -0,0 +1,21 @@
+// let patientId = '50';
+// let isEnrolled = true;
+// const patientInfo = {
+//   firstName: 'Ana',
+//   lastName: 'Santos',
+// };
+// const patientEmail = 'ana@email.com';
+// console.log(typeof patientId);
+// console.log(typeof isEnrolled);
+// console.log(typeof patientInfo);
+// console.log(typeof patientEmail);
+
+let base, height, area, perimeter;
+base = 5;
+height = 8;
+
+area = base * height;
+perimeter = (base*2)+(height*2);
+
+console.log(area);
+console.log(perimeter);
