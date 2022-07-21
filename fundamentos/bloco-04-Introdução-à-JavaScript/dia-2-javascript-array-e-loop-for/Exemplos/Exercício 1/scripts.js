8e2be6e470049ab5fe0bf1e99dbebf19c9108b54
victor-s-salles <victor.salles@engenharia.ufjf.br>
2022-07-21 15:48:05 -0300

    Iniciando os exercícios do dia 02, bloco 04

---
+++ fundamentos/bloco-04-Introdução-à-JavaScript/dia-2-javascript-array-e-loop-for/Exemplos/Exercício 1/scripts.js
@@ -0,0 +1,22 @@
+// Primeiro:---------------------------------------------------------------------------------
+
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let menuServices = menu[1];
+
+// console.log(menuServices);
+
+// Segundo: --------------------------------------------------------------------------------
+
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+
+// let indexOfPortfolio = menu.indexOf('Portfólio');
+
+// console.log(indexOfPortfolio);
+
+// Terceiro: --------------------------------------------------------------------------------
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+
+menu.push('Contato')
+
+console.log(menu);
