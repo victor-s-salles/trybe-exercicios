fb48f96184e77b233b85cbe0b5bc0b6c2025ac79
victor-s-salles <victor.salles@engenharia.ufjf.br>
2022-07-27 18:38:22 -0300

    Resolvendo exercício do conteudo part2

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/Exercícios do conteudo /script02.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/Exercícios do conteudo /script02.js
@@ -5,6 +5,16 @@ let emergency = document.getElementsByClassName('emergency-tasks')[0];
 emergency.style.backgroundColor = 'rgb(255, 159, 132)';
 
 
+let noEmergency = document.getElementsByClassName('no-emergency-tasks')[0];
+noEmergency.style.backgroundColor = 'yellow';
 
+for (let index = 0 ; index < document.querySelectorAll('.emergency-tasks h3').length ; index += 1)
+{let corEmergencia = document.querySelectorAll('.emergency-tasks h3')[index];
+corEmergencia.style.backgroundColor = 'purple';
+}
+for (let index = 0 ; index < document.querySelectorAll('.no-emergency-tasks h3').length ; index += 1)
+{let corEmergencia = document.querySelectorAll('.no-emergency-tasks h3')[index];
+corEmergencia.style.backgroundColor = 'black';
+}
 
 
