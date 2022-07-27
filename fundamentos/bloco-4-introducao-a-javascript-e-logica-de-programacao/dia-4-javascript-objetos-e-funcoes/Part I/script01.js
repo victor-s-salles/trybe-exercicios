302a3e94bd78e4690ba1cec03ec4e5072000b643
victor-s-salles <victor.salles@engenharia.ufjf.br>
2022-07-27 14:50:07 -0300

    Atualização das pastas

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/Part I/script01.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/Part I/script01.js
@@ -1,8 +1,8 @@
 let info = {
-    personagem: 'Margarida',
-    origem: 'Pato Donald',
-    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-  };
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+};
 
 let info2 = {
   personagem: 'Tio Patinhas',
@@ -17,29 +17,29 @@ info.recorrente = 'Sim';
 
 console.log(info);
 
-for(let key in info){
-    console.log(key)
+for (let key in info) {
+  console.log(key)
 }
 
 console.log('----------------------------------------------')
 
-for(let key in info){
-    console.log(info[key])
+for (let key in info) {
+  console.log(info[key])
 }
 
 console.log('----------------------------------------------')
 
 
 for (let properties in info) {
-    if (
-      properties === 'recorrente' &&
-      info[properties] === 'Sim' &&
-      info2[properties] === 'Sim'
-    ) {
-      console.log('Ambos recorrentes');
-    } else {
-      console.log(info[properties] + ' e ' + info2[properties]);
-    }
+  if (
+    properties === 'recorrente' &&
+    info[properties] === 'Sim' &&
+    info2[properties] === 'Sim'
+  ) {
+    console.log('Ambos recorrentes');
+  } else {
+    console.log(info[properties] + ' e ' + info2[properties]);
   }
+}
 
 console.log('----------------------------------------------')
