07ff94d14e72ae7804d894836c24028f73a40196
victor-s-salles <victor.salles@engenharia.ufjf.br>
2022-07-27 17:49:09 -0300

    Resolvido parte dos exercícios do conteudo

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/Exercícios do conteudo /script02.js
@@ -0,0 +1,8 @@
+let header = document.getElementById('header-container');
+header.style.backgroundColor = 'rgb(0, 176, 105)';
+
+let emergency = document.getElementsByClassName('emergency-tasks')[0];
+emergency.style.backgroundColor = 'rgb(255, 159, 132)';
+
+
+
