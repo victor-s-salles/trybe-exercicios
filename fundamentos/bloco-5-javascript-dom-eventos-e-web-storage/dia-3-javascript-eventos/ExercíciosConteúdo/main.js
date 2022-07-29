f377f5d14d85f709d16581f7c2828278223bd8e3
victor-s-salles <victor.salles@engenharia.ufjf.br>
2022-07-29 15:51:38 -0300

    Feito os exercicios do Conteúdo

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/ExercíciosConteúdo/main.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/ExercíciosConteúdo/main.js
@@ -12,15 +12,56 @@ const myWebpage = document.getElementById('my-spotrybefy');
 // 2. Crie uma função que adicione a classe 'tech' ao elemento `li` quando for clicado.
 // 2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
 
+function changeTech(event){
+    let techElement = document.querySelector('.tech');
+    techElement.classList.remove('tech');
+    event.target.classList.add('tech');
+}
+firstLi.addEventListener('click', changeTech);
+secondLi.addEventListener('click', changeTech);
+thirdLi.addEventListener('click', changeTech);
+
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech';
+
+input.addEventListener('input', changeText) 
+
+function changeText(event)
+{
+    let techElement = document.querySelector('.tech');
+    techElement.innerText = event.target.value;
+
+};
 
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portfólio?
 
+myWebpage.addEventListener('click', minhaPag);
+
+function minhaPag (){
+    
+    window.location.replace('https://victor-s-salles.github.io/');
+}
+
+
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
+
+myWebpage.addEventListener("mouseover", mudarCor);
+function mudarCor(event) {
+
+    event.target.style.color = 'black';
+
+}
+
+myWebpage.addEventListener("mouseleave", mudarCor2);
+function mudarCor2(event) {
+
+    event.target.style.color = 'red';
+
+}
+
 
 // Segue abaixo um exemplo do uso de event.target:
 
@@ -33,6 +74,8 @@ function resetText(event) {
 }
 
 firstLi.addEventListener('dblclick', resetText);
+secondLi.addEventListener('dblclick', resetText);
+thirdLi.addEventListener('dblclick', resetText);
 
 // Não precisa passar o parâmetro dentro da callback resetText. O próprio
 // navegador fará esse trabalho por você, não é legal? Desse jeito, o
