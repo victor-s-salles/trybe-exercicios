709be51821cab335b262b134c375cc837bb8080c
victor-s-salles <victor.salles@engenharia.ufjf.br>
2022-08-17 11:41:51 -0300

    Resolvendo o bõnus

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-3-primeiros-passos-em-jest/sum.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-3-primeiros-passos-em-jest/sum.js
@@ -87,5 +87,51 @@ function myRemove(arr, item) {
     }
 
 
+    // Dados
+const professionalBoard = [
+  {
+    id: '8579-6',
+    firstName: 'Ana',
+    lastName: 'Gates',
+    specialities: ['UX', 'Design'],
+  },
+  {
+    id: '5569-4',
+    firstName: 'George',
+    lastName: 'Jobs',
+    specialities: ['Frontend', 'Redux', 'React', 'CSS'],
+  },
+  {
+    id: '4456-4',
+    firstName: 'Leila',
+    lastName: 'Zuckerberg',
+    specialities: ['Context API', 'RTL', 'Bootstrap'],
+  },
+  {
+    id: '1256-4',
+    firstName: 'Linda',
+    lastName: 'Bezos',
+    specialities: ['Hooks', 'Context API', 'Tailwind CSS'],
+  },
+  {
+    id: '9852-2-2',
+    firstName: 'Jeff',
+    lastName: 'Cook',
+    specialities: ['Ruby', 'SQL'],
+  },
+  {
+    id: '4678-2',
+    firstName: 'Paul',
+    lastName: 'Dodds',
+    specialities: ['Backend'],
+  },
+];
+
+// Pesquisa
+const searchEmployee = (id, detail) => {
+  // Implemente seu código aqui
+};
+
+
   
-  module.exports = {sum, myRemove, myFizzBuzz, encode, decode, techList, hydrate};
+  module.exports = {sum, myRemove, myFizzBuzz, encode, decode, techList, hydrate, searchEmployee};
--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-3-primeiros-passos-em-jest/sum.test.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-3-primeiros-passos-em-jest/sum.test.js
@@ -1,4 +1,4 @@
-const {sum, myRemove, myFizzBuzz, encode, decode, techList, hydrate} = require('./sum')
+const {sum, myRemove, myFizzBuzz, encode, decode, techList, hydrate, searchEmployee} = require('./sum')
 
 describe('teste da função sum', () => {
     it('deve retorna o resultado correto das somas', () =>{
@@ -112,5 +112,42 @@ describe('Testa a função hydrate', () => {
     expect(hydrate('2 shots de tequila, 2 cervejas e 1 corote')).toBe('5 copos de água');
     expect(hydrate('1 copo de catuaba, 1 cervejas e 1 copo de vinho')).toBe('3 copos de água');
     expect(hydrate('4 caipirinhas e 2 cervejas')).toBe('6 copos de água');
+  });
+});
+
+
+// ===================================== BÔNUS ==============================================
+
+describe('Testa a função searchEmployee', () => {
+  it('Testa se searchEmployee é uma função', () => {
+    expect(typeof searchEmployee).toBe('function');
+  });
+  it('Testa se searchEmployee(id, "firstName") retorna o primeiro nome do usuário da id consultada', () => {
+    const actual = searchEmployee('1256-4', 'firstName');
+    const expected = 'Linda';
+
+    expect(actual).toBe(expected);
+  });
+  it('Testa se searchEmployee(id, "lastName") retorna o segundo nome do usuário da id consultada', () => {
+    const actual = searchEmployee('1256-4', 'lastName');
+    const expected = 'Bezos';
+
+    expect(actual).toBe(expected);
+  });
+  it('Testa se searchEmployee(id, "specialities") retorna um array com todas as habilidades do id pesquisado', () => {
+    const actual = searchEmployee('1256-4', 'specialities');
+    expect(actual).toContain('Hooks', 'Context API', 'Tailwind CSS');
+  });
+  it('Testa se um erro com a mensagem "ID não identificada" é retornado quando a ID não existir', () => {
+    expect(() => { searchEmployee('1256-8', 'specialities') }).toThrow();
+  });
+  it('Testa a mensagem do erro para ID inexistente', () => {
+    expect(() => { searchEmployee('1256-8', 'specialities') }).toThrowError(new Error("ID não identificada"));
+  });
+  it('Testa se lança um erro quando a informação e o ID são inexistentes', () => {
+    expect(() => { searchEmployee() }).toThrow();
+  });
+  it('Testa a mensagem do erro para informação inexistente', () => {
+    expect(() => { searchEmployee('4678-2', 'shift') }).toThrowError(new Error("Informação indisponível"));
   });
 });
