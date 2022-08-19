0f1d6d2d7c6939b2133251ef49a645b8a92c7bd3
victor-s-salles <victor.salles@engenharia.ufjf.br>
2022-08-19 15:37:04 -0300

    Feito o exemplo do course

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/Course/FixaçãoCourse.js
@@ -0,0 +1,17 @@
+const acorda = () => 'Acordando!!';
+
+const cafe = () => 'Bora tomar café!!';
+
+const dormir = () => 'Partiu dormir!!';
+
+// const doingThings = (func) => {
+//     const result = func();
+//     return result;
+//   };
+
+const doingThings = (func) => func();
+
+
+
+
+console.log(doingThings(cafe));
