9a69754a58977e9cf56ab82e09d04be8b77725d2
victor-s-salles <victor.salles@engenharia.ufjf.br>
2022-08-22 19:00:59 -0300

    Feito o exercicio 7

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercise7.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercise7.js
@@ -62,3 +62,16 @@ const books = [
   ];
   
   // Adicione o código do exercício aqui:
+
+  const expectedResult = false;
+
+function authorUnique() {
+  return books.every( (book) => {
+    !books.some((bookSome) => {
+        ((bookSome.author.birthYear === book.author.birthYear)
+        && (bookSome.author.name !== book.author.name))
+    })
+  })
+}
+
+console.log(authorUnique());
