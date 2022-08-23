0cc14b813a097ed2d2f841a9118016164a85bb3f
victor-s-salles <victor.salles@engenharia.ufjf.br>
2022-08-23 18:46:30 -0300

    Feito o exercicio 3

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercise3.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercise3.js
@@ -62,3 +62,21 @@ const books = [
   ];
   
   // Adicione o código do exercício aqui:
+
+  const expectedResult = {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: {
+      name: 'George R. R. Martin',
+      birthYear: 1948,
+    },
+    releaseYear: 1991,
+  };
+  
+  function getNamedBook() {
+    // escreva seu código aqui
+    return books.find((book) => book.name.length === 26)
+  }
+
+  console.log(getNamedBook());
