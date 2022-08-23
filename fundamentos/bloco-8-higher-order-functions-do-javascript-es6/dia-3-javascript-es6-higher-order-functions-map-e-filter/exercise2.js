4b7c85ff9ccae84c52ddd3ee88b20c4d5ebd3aa1
victor-s-salles <victor.salles@engenharia.ufjf.br>
2022-08-23 15:57:11 -0300

    Feito o exercicio 2

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercise2.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercise2.js
@@ -62,3 +62,42 @@ const books = [
   ];
   
   // Adicione o código do exercício aqui:
+
+  const expectedResult = [
+    {
+      age: 31,
+      author: 'Isaac Asimov',
+    },
+    {
+      age: 38,
+      author: 'H. P. Lovecraft',
+    },
+    {
+      age: 39,
+      author: 'Stephen King',
+    },
+    {
+      age: 43,
+      author: 'George R. R. Martin',
+    },
+    {
+      age: 45,
+      author: 'Frank Herbert',
+    },
+    {
+      age: 62,
+      author: 'J. R. R. Tolkien',
+    },
+  ];
+  
+  function nameAndAge() {
+    // escreva seu código aqui
+    return books.map((book) => (
+      {
+        author: book.author.name,
+        age: book.releaseYear - book.author.birthYear,
+      }
+    )).sort((obj1, obj2) => obj1.age - obj2.age);
+  }
+
+  console.log(nameAndAge());
