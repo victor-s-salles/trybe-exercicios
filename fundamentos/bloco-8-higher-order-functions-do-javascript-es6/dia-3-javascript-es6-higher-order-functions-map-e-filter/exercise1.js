cb008e7a5dbb8832ced61adf2be63f8459be988f
victor-s-salles <victor.salles@engenharia.ufjf.br>
2022-08-23 15:33:55 -0300

    Feito o exercicio 1

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercise1.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercise1.js
@@ -62,3 +62,19 @@ const books = [
   ];
   
   // Adicione o código do exercício aqui:
+
+  const expectedResult = [
+    'As Crônicas de Gelo e Fogo - Fantasia - George R. R. Martin',
+    'O Senhor dos Anéis - Fantasia - J. R. R. Tolkien',
+    'Fundação - Ficção Científica - Isaac Asimov',
+    'Duna - Ficção Científica - Frank Herbert',
+    'A Coisa - Terror - Stephen King',
+    'O Chamado de Cthulhu - Terror - H. P. Lovecraft',
+  ];
+  
+  function formatedBookNames() {
+    const newArray = books.map((elemento) => `${elemento.name} - ${elemento.genre} - ${elemento.author.name}` )
+    return newArray;
+  }
+
+  console.log(formatedBookNames());
