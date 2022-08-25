51e284a9f8925f21230c95f81a71fa66503aa30b
victor-s-salles <victor.salles@engenharia.ufjf.br>
2022-08-25 19:01:21 -0300

    Feito o exercicio 4

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercise4.js
@@ -0,0 +1,38 @@
+
+const people = [
+    {
+      name: 'Nicole',
+      bornIn: 1992,
+      nationality: 'Australian',
+    },
+    {
+      name: 'Harry',
+      bornIn: 2008,
+      nationality: 'Australian',
+    },
+    {
+      name: 'Toby',
+      bornIn: 1901,
+      nationality: 'Australian',
+    },
+    {
+      name: 'Frida',
+      bornIn: 1960,
+      nationality: 'Dannish',
+    },
+    {
+      name: 'Fernando',
+      bornIn: 2001,
+      nationality: 'Brazilian',
+    },
+  ];
+  //bornIn: nascido em
+  
+  // escreva filterPeople abaixo
+
+  const filterPeople = (array) => array.filter(
+    ({ nationality, bornIn }) =>
+    nationality === 'Australian'  && bornIn > 1900 && bornIn <= 2000,
+  );
+
+  console.log(filterPeople(people));
