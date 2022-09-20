1f6222135ac71ccc58a2ac4de7f4bf598d6a51e4
victor-s-salles <victor.salles@engenharia.ufjf.br>
2022-09-20 19:39:47 -0300

    Feito exercicio do dia

---
+++ front-end/Introducao-ao-React-02/Dia-02-Componetes-React/exercise-image-component/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+import Image from './Image';
+
+function App() {
+  const URL = 'https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg';
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <Image source={ URL } alternativeText="Cute cat staring"></Image>
+      </header>
+    </div>
+  );
+}
+
+export default App;
