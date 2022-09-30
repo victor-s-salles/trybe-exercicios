14fa1a4db336f5b0277a961de385956f85a1ffdc
victor-s-salles <victor.salles@engenharia.ufjf.br>
2022-09-30 18:46:05 -0300

    Exercicios de fixação do dia

---
+++ front-end/Ciclo-de-Vida-de-Componentes-React-Router-04/dia-02-React-Router/exemplo-history/src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class Home extends React.Component {
+  render() {
+    const { history } = this.props;
+    return (
+      <>
+        <h1>Home Page</h1>
+        <button
+          type="button"
+          onClick={ () => history.push('/welcome') }
+        >
+          Acesse a página de Boas-Vindas
+        </button>
+      </>
+    );
+  }
+}
+
+export default Home;
